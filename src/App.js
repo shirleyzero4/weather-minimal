67464ca18e1a177aa9de0e264b3ec2ff71522a08
Shirley Nguyen <shirleyzero4@gmail.com>
2023-01-11 23:16:32 -0800

    Import axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, { useState }  from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faChevronDown, faChevronUp, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronUp, faCircle, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=a86ac6f164e80a61d42fa9ea208dbe2b`
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
   return (
     <div>
       <header>
-        <div className='menu'>
-          <FontAwesomeIcon icon={faBars} />
+        <div className='search'>
+          <div className='icon'>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
         </div>
         <div className='location'>Banning</div>
         <div className='units'>C°/F°</div>
